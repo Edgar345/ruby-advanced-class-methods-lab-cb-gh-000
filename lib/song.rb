dce51f16a33b012d3c154a2bdc7e0bed67ab65b8
Edgar V <evenegas9@ucmerced.edu>
2019-09-12 08:12:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,9 @@ class Song
   end
 
   def self.find_or_create_by_name name
-    found_by_name? = self.find_by_name(name)
-    if (found_by_name?)
-      found_by_name?
+    found_by_name = self.find_by_name(name)
+    if (found_by_name)
+      found_by_name
     else
       self.create_by_name(name)
     end
